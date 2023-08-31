900f7e25da671b07d423890bbb95f895e0b7a5cc
shashank <shashankssinghal@gmail.com>
2023-08-31 14:52:13 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
             System.out.println("Hello my first git code");
+            System.out.println("second commit");
         }
     }
 }
